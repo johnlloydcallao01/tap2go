42ddb24307cc192ec0c46973e50ccc2dd9d71060
John Lloyd Callao <johnlloydcallao@gmail.com>
2025-06-22 00:21:05 +0800

    Remove Next.js build cache from repository and update .gitignore

--- apps/web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"i78T71vedJULngbGtSTf3UMfPx5V7DSsOND4UIE53Io=\"\n}"
--- apps/web/.next/static/chunks/apps_web_src_app_(customer)_home_page_tsx_75df79cf._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/apps_web_src_285ed0f3._.js",
-  "static/chunks/3c0f5_93524d68._.js"
-],
-    source: "dynamic"
-});
