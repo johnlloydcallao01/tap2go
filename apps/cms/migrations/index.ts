eb710d95603876c5cbb17e61e6e11b905390705f
John Lloyd Callao <johnlloydcallao@gmail.com>
2025-08-15 18:53:36 +0800

    Err

--- apps/cms/migrations/index.ts
+++ apps/cms/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250815_100800 from './20250815_100800';
+import * as migration_20250815_clean_users_table from './20250815_clean_users_table';
 
 export const migrations = [
   {
-    up: migration_20250815_100800.up,
-    down: migration_20250815_100800.down,
-    name: '20250815_100800'
+    up: migration_20250815_clean_users_table.up,
+    down: migration_20250815_clean_users_table.down,
+    name: '20250815_clean_users_table'
   },
 ];
